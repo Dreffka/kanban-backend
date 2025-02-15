7cbff5a1089e5b94ddd8d821faaa23fb4aca9a51
v.dordiuk <v.dordiuk@urancompany.com>
2025-02-15 19:54:06 +0200

    update knex

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,19 @@
-// Update with your config settings.
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+const commonConfig = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    directory: './migrations'
   }
+};
 
+module.exports = {
+  development: { ...commonConfig },
+  production: { ...commonConfig }
 };
